9d6fbe93c54ef51a62bae9f095377e94c06b2051
Pranath <pranath7@gmail.com>
2026-07-21 18:11:45 +0530

    hotfix: remove crossorigin=anonymous from product images (was breaking ALL Cloudinary images on site), update shipping text to tiered

--- app.js
+++ app.js
@@ -857,7 +857,7 @@ function renderProducts(filter) {
                   <svg viewBox="0 0 24 24" fill="${isWL ? 'currentColor' : 'none'}" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78Z"/></svg>
                 </button>
     <div class="p-img">
-      <img src="${clOpt(p.img, 600)}" alt="${p.name}" loading="lazy" onerror="this.onerror=null;this.src='${p.img}'" crossorigin="anonymous">
+      <img src="${clOpt(p.img, 600)}" alt="${p.name}" loading="lazy" onerror="this.onerror=null;this.src='${p.img || ''}'" >
       ${quickActionHtml}
     </div>
                 <div class="p-info">
